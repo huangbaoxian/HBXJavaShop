b4c82715a1f34d5eda7dc3224e36cb1c2d2d2ed9
huangbx <huangbx@example.com>
2019-05-22 10:35:46 +0800

    新建工程

---
+++ src/main/java/com/hbx/shop/controller/BaseController.java
@@ -0,0 +1,18 @@
+package com.hbx.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class BaseController {
+
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String start(){
+        return "start";
+    }
+
+}
