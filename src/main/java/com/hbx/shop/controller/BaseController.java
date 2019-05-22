6b17c0abcd8d7c92dee1ab26492d8b5474fc874f
huangbx <huangbx@example.com>
2019-05-22 15:02:02 +0800

    新增文件

--- src/main/java/com/hbx/shop/controller/BaseController.java
+++ src/main/java/com/hbx/shop/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.hbx.shop.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +10,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BaseController {
 
+    /*
+    * *@Value("${weixin.pay.notifyUrl}")
+    String weixinNotifyUrl;
+    * */
+
+    @Value("${test.str}")
+    String testStr;
+
+
     @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String start(){
-        return "start";
+        System.out.print("start");
+        System.out.print(testStr);
+        String str = "new start" + testStr;
+        return str;
+
     }
 
 }
